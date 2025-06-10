eb3aa3e2473cad0e6b491a282e7a59f69124d461
kotjuz <rkotarski10@gmail.com>
2025-06-10 08:36:08 +0200

    feat: created example files

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+var wordList = []string{
+	"apple", "banana", "orange", "grape", "peach",
+	"dog", "cat", "mouse", "rabbit", "fox",
+	"book", "pen", "notebook", "paper", "pencil",
+}
+
+func main() {
+	GenerateFiles(wordList)
+}
